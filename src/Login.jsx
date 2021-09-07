fdb3099652a7f9d1862d691b3f8aa922904f03b1
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2021-09-08 03:27:35 +0530

    CSS of login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,17 @@
+import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Login(){
     return<div>
-        <img class="wave" src="img/wave.png"/>
+        <img class="wave" src="https://github.com/sefyudem/Responsive-Login-Form/blob/master/img/wave.png?raw=true"/>
 	<div class="container">
 		<div class="img">
-			<img src="img/bg.svg"/>
+			<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/bg.svg"/>
 		</div>
 		<div class="login-content">
 			<form action="index.html">
-				<img src="img/avatar.svg"/>
+				<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/avatar.svg"/>
 				<h2 class="title">Welcome</h2>
            		<div class="input-div one">
            		   <div class="i">
