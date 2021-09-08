8fb5876901403f92e4ac91da89230f1e12c0aba5
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2021-09-08 18:01:47 +0530

    Login Card view

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,36 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Login(){
-    return<div>
-        <img class="wave" src="https://github.com/sefyudem/Responsive-Login-Form/blob/master/img/wave.png?raw=true"/>
-	<div class="container">
-		<div class="img">
-			<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/bg.svg"/>
+    return<div className="LoginCard">
+        <img className="wave" alt ="" src="https://github.com/sefyudem/Responsive-Login-Form/blob/master/img/wave.png?raw=true"/>
+	<div className="container">
+		<div className="img">
+			<img alt ="" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/bg.svg"/>
 		</div>
-		<div class="login-content">
+		<div className="login-content">
 			<form action="index.html">
-				<img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/avatar.svg"/>
-				<h2 class="title">Welcome</h2>
-           		<div class="input-div one">
-           		   <div class="i">
-           		   		<i class="fas fa-user"></i>
+				<img alt ="" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/82b8d8efd3b0ac6382b9d0d71a99c6cf9dcefa23/img/avatar.svg"/>
+				<h2 className="title">Welcome</h2>
+           		<div className="input-div one">
+           		   <div className="i">
+           		   		<i className="fas fa-user"></i>
            		   </div>
-           		   <div class="div">
+           		   <div className="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" class="input"/>
+           		   		<input type="text" className="input"/>
            		   </div>
            		</div>
-           		<div class="input-div pass">
-           		   <div class="i"> 
-           		    	<i class="fas fa-lock"></i>
+           		<div className="input-div pass">
+           		   <div className="i"> 
+           		    	<i className="fas fa-lock"></i>
            		   </div>
-           		   <div class="div">
+           		   <div className="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" class="input"/>
+           		    	<input type="password" className="input"/>
             	   </div>
             	</div>
             	<a href="#">Forgot Password?</a>
-            	<input type="submit" class="btn" value="Login"/>
+            	<input type="submit" className="btn" value="Login"/>
+				<input type="submit" className="btn" value="Signup"/>
             </form>
         </div>
     </div>
