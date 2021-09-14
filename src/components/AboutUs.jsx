7b364b8c31002bbfdd70be2b38e640a2361e067a
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2021-09-14 21:57:11 +0530

    Day12 Added About us images

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -43,12 +43,12 @@ export default function About() {
             <div class="col-lg-4 col-md-6">
               <div class="card-box text-center">
                 <div class="user-pic">
-                  <img src="images/lab.jpg" class="img-fluid" alt="User Pic" />
+                  <img src="https://avatars.githubusercontent.com/u/62895849?v=4" class="img-fluid" alt="User Pic" />
                 </div>
                 <h2>Mohit Singhal</h2>
                 <h4>Developer & Designer</h4>
                 <hr />
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p>I'm Mohit Singhal, a passionate coder, and a tech enthusiast well-versed in Programming (C++, Java, JavaScript) with an interest in Software Development and Designing (UI/UX). A CS undergraduate who loves to explore new things. I've Started exploring MERN Stack (MongoDB, Express, React, Node.Js) and learning DSA as well.</p>
                 <hr />
                 <button class="btn btn-primary btn-lg btn-dark" type="button">
                   Sign Up
@@ -60,7 +60,7 @@ export default function About() {
               <div class="card-box text-center">
                 <div class="user-pic">
                   <img
-                    src="images/Mastiff.jpg"
+                    src="https://avatars.githubusercontent.com/u/55994328?v=4"
                     class="img-fluid"
                     alt="User Pic"
                   />
